14f19957548d3aeba79982c205fd38fd7ea86ef2
Ashley Ginsberg <aginz200@gmail.com>
2015-05-20 23:36:16 -0400

    Removed user authentication requirement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to  edit_user_path(current_user)
+      redirect_to edit_user_path(current_user)
     end
   end
 end
